6ee2afeae78522bba14b7debe78505720639321f
Diego Eis <diegoeis@gmail.com>
2016-03-22 12:48:24 -0300

    Separating Sidebar file And calling sidebar to single file.

---
+++ wp-content/themes/tableless/sidebar.php
@@ -0,0 +1,5 @@
+<aside class="tb-sidebar">
+  <?php if ( dynamic_sidebar( 'sidebar' ) ) : ?>
+      <!-- Just do Something! -->
+  <?php endif;?>
+</aside>
---
+++ wp-content/themes/tableless/single.php
@@ -0,0 +1,1 @@
+    <?php get_sidebar();?>
