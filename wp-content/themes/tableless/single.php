eba38120dc4f33af9d5744ec1002179ac20bc058
Diego Eis <diegoeis@gmail.com>
2016-03-22 17:48:07 -0300

    Adding real content to articles

--- wp-content/themes/tableless/single.php
+++ wp-content/themes/tableless/single.php
@@ -14,7 +14,7 @@
         <header>
          
           <h1>
-            Criando efeitos de página em livros com front-end
+            <?php the_title();?>
           </h1>
           <span class="tb-author-info">
             <?php echo get_avatar( get_the_author_id() , 100 ); ?>
@@ -22,31 +22,7 @@
           </span>
         </header>
         
-        <p>
-          Caros amigos, a execução dos pontos do programa nos obriga à análise das diversas correntes de pensamento. No mundo atual, a complexidade dos estudos efetuados representa uma abertura para a melhoria das posturas dos órgãos dirigentes com relação às suas atribuições.
-        </p>
-        <p>
-          Evidentemente, <a href="#">a valorização de fatores subjetivos ainda</a> não demonstrou convincentemente que vai participar na mudança de todos os recursos funcionais envolvidos. Não obstante, a estrutura atual da organização auxilia a preparação e a composição dos relacionamentos verticais entre as hierarquias.
-        </p>
-        <p>
-          Acima de tudo, é fundamental ressaltar que o <a href="#">desenvolvimento contínuo de distintas formas de</a> atuação causa impacto indireto na reavaliação das novas proposições.
-        </p>
-        <h2>Confira abaixo o código completo</h2>
-        <p>
-          As experiências acumuladas demonstram que a crescente influência da mídia é uma das consequências do impacto na agilidade decisória.
-        </p>
-        <ul>
-          <li><a href="#">Uma Opção</a></li>
-          <li><a href="#">Duas Opção</a></li>
-          <li><a href="#">Três Opção</a></li>
-          <li><a href="#">Quatro Opção</a></li>
-        </ul>
-        <p>
-          O empenho em analisar a percepção das dificuldades estimula a padronização das diretrizes de desenvolvimento para o futuro. Do mesmo modo, a consolidação das estruturas estende o alcance e a importância das condições inegavelmente apropriadas. 
-        </p>
-        <p>
-          Gostaria de enfatizar que a mobilidade dos capitais internacionais oferece uma interessante oportunidade para verificação do levantamento das variáveis envolvidas. Ainda assim, existem dúvidas a respeito de como o início da atividade geral de formação de atitudes cumpre um papel essencial na formulação dos procedimentos normalmente adotados.
-        </p>
+        <?php the_content();?>
 
         <footer>
             <span class="tb-post-time">Publicado no dia <time datetime="<?php the_time('Y-m-d g:i') ?>"> <?php the_time('j') ?> de <?php the_time('F') ?> de <?php the_time('Y') ?></time></span>
