4da244592b9da1c9d205f8a2f988f695c87322e1
Diego Eis <diegoeis@gmail.com>
2016-03-25 01:54:54 -0300

    little changes in home

--- wp-content/themes/tableless/search.php
+++ wp-content/themes/tableless/search.php
@@ -7,15 +7,9 @@
       <h3 class="tb-title-1"><?php printf( __( 'Resultado da busca por %s', 'tableless' ), '<span>' . get_search_query() . '</span>' ); ?></h3>
     </header>
 
-    <div class="tb-search-results">
+  <div class="tb-search-results">
   <?php while(have_posts()) : the_post(); ?>
-    <a href="<?php the_permalink();?>" class="tb-box">
-      <?php if(has_post_thumbnail()) :?>
-        <figure><?php echo get_the_post_thumbnail( $post_id, 'medium' ); ?></figure>
-      <?php endif;?>
-      <time class="tb-post-time" datetime="<?php the_time('Y-m-d g:i') ?>"> <?php the_time('j') ?> <?php the_time('M') ?> <?php the_time('Y') ?></time>
-      <h2><?php the_title();?></h2>
-    </a>
+    <?php get_template_part('tb-box');?>
   <?php endwhile; ?>
   </div>
 
