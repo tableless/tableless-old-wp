3043db76dd7cdccda152c66872ba90a3a4b64eda
Diego Eis <diegoeis@gmail.com>
2016-03-27 19:19:03 -0300

    adding gulpfile in root

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,17 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
-  gulp.src('./**/*.sass')
+  gulp.src('./wp-content/themes/tableless/**/*.sass')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('./wp-content/themes/tableless/'))
 });
 
 gulp.task('default', function () {
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./**/*.sass', ['sass']);
+  gulp.watch('./wp-content/themes/tableless/**/*.sass', ['sass']);
 });
