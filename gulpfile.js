025ca4c0d4c15f8bdc8070dd7741444b7d669f77
Diego Eis <diegoeis@gmail.com>
2016-04-03 00:12:06 -0300

    endereço da pasta do theme em variavel para facilitar no gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 'use strict';
 
+var themeTableless = "./wp-content/themes/tableless/"
+var uploadsFolder = "./wp-content/uploads/"
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
-  gulp.src('./wp-content/themes/tableless/**/*.sass')
+  gulp.src(themeTableless + '**/*.sass')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(sass({outputStyle: 'compressed'}))
