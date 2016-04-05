8ceca87a10534f512e8a2624c2f16af5015abb64
Diego Eis <diegoeis@gmail.com>
2016-04-04 22:41:57 -0300

    Author page. Fix #8

--- wp-content/themes/tableless/author.php
+++ wp-content/themes/tableless/author.php
@@ -1,31 +1,28 @@
 <?php get_header();?>
  
-<section class="tb-section tb-category">
+<section class="tb-author-page">
 <div class="tb-container">
-
   
-      <div class="tb-post-author">
-        <?php echo get_avatar( get_the_author_id() , 120 ); ?>
-        <h3><?php the_author(); ?></h3>
-        <p><?php the_author_meta('description'); ?></p>
-        <p><a href="<?php bloginfo('url'); ?>/?author=<?php the_author_ID(); ?>">Veja mais artigos deste autor</a></p>
-      </div>
+  <div class="tb-post-author">
+    <?php echo get_avatar( get_the_author_id() , 600 ); ?>
+    <h3 class="tb-title-section"><?php the_author(); ?></h3>
+    <p class="tb-lead-paragraph tb-divider"><?php the_author_meta('description'); ?></p>
+  </div>
 
+  <div class="tb-box-list">
+    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 
-<section class="tb-box-list">
-  <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+      <?php get_template_part('tb-box');?>
 
-    <?php get_template_part('tb-box');?>
+    <?php endwhile; endif; wp_reset_query();?>
+  </div>
 
-  <?php endwhile; endif; wp_reset_query();?>
-</section>
-
-<div class="tb-pagination">
-<?php if (  $wp_query->max_num_pages > 1 ) : ?>
-  <?php next_posts_link( __( '&larr; Posts antigos', 'twentyten' ) ); ?>
-  <?php previous_posts_link( __( 'Posts recentes &rarr;', 'twentyten' ) ); ?>
-<?php endif; ?>
-</div>
+  <div class="tb-pagination">
+  <?php if (  $wp_query->max_num_pages > 1 ) : ?>
+    <?php next_posts_link( __( '&larr; Posts antigos', 'twentyten' ) ); ?>
+    <?php previous_posts_link( __( 'Posts recentes &rarr;', 'twentyten' ) ); ?>
+  <?php endif; ?>
+  </div>
 
 
 </div>
