acd2052b29fefad150aaa3726e4ba4f7a222a373
Lucas Lima Muller <contato@lucaslimamuller.com.br>
2016-04-06 15:54:38 +0000

    Arquivo 404.php criado com base na estrutura da página inicial

---
+++ wp-content/themes/tableless/404.php
@@ -0,0 +1,38 @@
+<?php get_header();?>
+ 
+<section class="tb-section">
+<div class="tb-container">
+
+
+    <h2 class="tb-title-section tb-align-center">
+      Desculpe, a página que você está procurando não existe
+    </h2>
+   <p class="tb-lead-paragraph tb-divider tb-align-center">Veja abaixo alguns posts que você pode gostar</p>
+    
+</section>
+<section class="tb-latest-posts no-divider">
+  
+    <div class="tb-container">
+  
+      <?php
+        $latestPostsargs = array(
+          'posts_per_page' => 8, // Quantidade de posts
+          'order'=> 'DESC',
+        );
+        $latestPosts = get_posts( $latestPostsargs );
+        foreach ( $latestPosts as $post ) : setup_postdata( $post ); ?>
+  
+          <?php get_template_part('tb-box');?>
+  
+  
+        <?php endforeach; wp_reset_postdata(); ?>
+  
+  
+    </div>
+        <a href="<?php echo get_page_link(42486); ?>" class="tb-btn-big">Mais artigos</a>
+  
+  </section>
+
+
+
+<?php get_footer();?>
