3eb006d39198601487dd14b1be448f7545588c44
Augusto Moura <augusto.borgesm@gmail.com>
2016-04-29 12:18:21 -0300

    Troca do nome de variáveis para ser mais legível em português

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 'use strict';
 
-var themeTableless = "./wp-content/themes/tableless/";
-var jsFolder = "./wp-content/themes/tableless/assets/js/";
+var paths = {
+  PASTA_TEMA: "./wp-content/themes/tableless/",
+  PASTA_JS: "./wp-content/themes/tableless/assets/js/"
+};
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
@@ -10,26 +12,26 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
  
 gulp.task('compilar-sass', function () {
-  gulp.src(themeTableless + '**/*.sass')
+  gulp.src(paths.PASTA_TEMA + '**/*.sass')
     .pipe(sourcemaps.init())
       .pipe(sass().on('error', sass.logError))
       .pipe(sass({outputStyle: 'compressed'}))
     .pipe(sourcemaps.write('maps'))
-    .pipe(gulp.dest('./wp-content/themes/tableless/'));
+    .pipe(gulp.dest(paths.PASTA_TEMA));
 });
 
-gulp.task('compilar-js', function() {
-  gulp.src(jsFolder + 'scripts.js')
+gulp.task('comprimir-js', function() {
+  gulp.src(paths.PASTA_JS + 'scripts.js')
     .pipe(sourcemaps.init())
       .pipe(uglify({preserveComments: 'license'}))
       .pipe(rename({suffix: '.min'}))
     .pipe(sourcemaps.write('maps'))
-    .pipe(gulp.dest(jsFolder));
+    .pipe(gulp.dest(paths.PASTA_JS));
 });
 
 gulp.task('watch', function () {
-  gulp.watch(themeTableless + '**/*.sass', ['compilar-sass']);
-  gulp.watch(jsFolder + 'scripts.js', ['compilar-js']);
+  gulp.watch(paths.PASTA_TEMA + '**/*.sass', ['compilar-sass']);
+  gulp.watch(paths.PASTA_JS + 'scripts.js', ['comprimir-js']);
 });
 
-gulp.task('default', ['compilar-sass', 'compilar-js', 'watch']);
+gulp.task('default', ['compilar-sass', 'comprimir-js', 'watch']);
