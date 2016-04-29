e1acc5eb994f65f40c322cc482a92d6e1ff55fd8
Augusto Moura <augusto.borgesm@gmail.com>
2016-04-29 11:25:29 -0300

    Primeiro polimento do gulpfile e troca do compressor jsmin pelo uglifyjs

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,36 @@
 'use strict';
 
-var themeTableless = "./wp-content/themes/tableless/"
-var jsFolder = "./wp-content/themes/tableless/assets/js/"
-var cssFolder = "./wp-content/themes/tableless/assets/css/"
+var themeTableless = "./wp-content/themes/tableless/";
+var jsFolder = "./wp-content/themes/tableless/assets/js/";
+var cssFolder = "./wp-content/themes/tableless/assets/css/";
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-var jsmin = require('gulp-jsmin');
+var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
  
 gulp.task('sass', function () {
   gulp.src(themeTableless + '**/*.sass')
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./wp-content/themes/tableless/'))
+      .pipe(sass().on('error', sass.logError))
+      .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sourcemaps.write('maps'))
+    .pipe(gulp.dest('./wp-content/themes/tableless/'));
 });
 
 gulp.task('jsmin', function() {
   gulp.src(jsFolder + 'scripts.js')
-  .pipe(jsmin())
-  .pipe(rename({suffix: '.min'}))
-  .pipe(gulp.dest(jsFolder));
-})
+    .pipe(sourcemaps.init())
+      .pipe(uglify({preserveComments: 'license'}))
+      .pipe(rename({suffix: '.min'}))
+    .pipe(sourcemaps.write('maps'))
+    .pipe(gulp.dest(jsFolder));
+});
 
 gulp.task('watch', function () {
   gulp.watch(themeTableless + '**/*.sass', ['sass']);
   gulp.watch(jsFolder + 'scripts.js', ['jsmin']);
 });
 
-gulp.task('default', ['watch'])
+gulp.task('default', ['compilar-sass', 'compilar-js', 'watch']);
