a4eca795e4ce3b022f6c05f2d1ae6389819ceb76
Augusto Moura <augusto.borgesm@gmail.com>
2016-05-01 21:12:19 -0300

    O nome das tasks e dos paths voltarão à ser em inglês para condizer com o restante do projeto

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var paths = {
-  PASTA_TEMA: "./wp-content/themes/tableless/",
-  PASTA_JS: "./wp-content/themes/tableless/assets/js/"
+  THEME_DIR: "./wp-content/themes/tableless/",
+  JS_DIR: "./wp-content/themes/tableless/assets/js/"
 };
 
 var gulp = require('gulp');
@@ -11,27 +11,27 @@ var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
  
-gulp.task('compilar-sass', function () {
-  gulp.src(paths.PASTA_TEMA + '**/*.sass')
+gulp.task('compile-sass', function () {
+  gulp.src(paths.THEME_DIR + '**/*.sass')
     .pipe(sourcemaps.init())
       .pipe(sass().on('error', sass.logError))
       .pipe(sass({outputStyle: 'compressed'}))
     .pipe(sourcemaps.write('maps'))
-    .pipe(gulp.dest(paths.PASTA_TEMA));
+    .pipe(gulp.dest(paths.THEME_DIR));
 });
 
-gulp.task('comprimir-js', function() {
-  gulp.src(paths.PASTA_JS + 'scripts.js')
+gulp.task('compress-js', function() {
+  gulp.src(paths.JS_DIR + 'scripts.js')
     .pipe(sourcemaps.init())
       .pipe(uglify({preserveComments: 'license'}))
       .pipe(rename({suffix: '.min'}))
     .pipe(sourcemaps.write('maps'))
-    .pipe(gulp.dest(paths.PASTA_JS));
+    .pipe(gulp.dest(paths.JS_DIR));
 });
 
 gulp.task('watch', function () {
-  gulp.watch(paths.PASTA_TEMA + '**/*.sass', ['compilar-sass']);
-  gulp.watch(paths.PASTA_JS + 'scripts.js', ['comprimir-js']);
+  gulp.watch(paths.THEME_DIR + '**/*.sass', ['compilar-sass']);
+  gulp.watch(paths.JS_DIR + 'scripts.js', ['comprimir-js']);
 });
 
-gulp.task('default', ['compilar-sass', 'comprimir-js', 'watch']);
+gulp.task('default', ['compile-sass', 'compress-js', 'watch']);
