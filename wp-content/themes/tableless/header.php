dd683d0ed4970c38f8ddbb0d503b52a2c0eb3383
Diego Eis <diegoeis@gmail.com>
2016-07-10 16:27:49 -0300

    testing json-ld

--- wp-content/themes/tableless/header.php
+++ wp-content/themes/tableless/header.php
@@ -2,9 +2,9 @@
 <html lang="pt-br" <?php body_class();?>>
 <head>
 
-<script type="application/ld+json">// <![CDATA[
-<?#php echo json_encode($payload); ?>
 <?php include('json-ld-article.php'); ?>
+<script type="application/ld+json">// <![CDATA[
+<?php echo json_encode($payload); ?>
 // ]]></script>
 </head>
   <title><?php wp_title( '|', true, 'right' ); ?></title>
