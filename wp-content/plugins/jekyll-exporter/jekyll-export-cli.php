83c62ba0a980393728c5d9536c488cf147db1d0b
Diego Eis <diego@tableless.com.br>
2017-04-01 20:11:52 -0300

    jekyll export

---
+++ wp-content/plugins/jekyll-exporter/jekyll-export-cli.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Exports WordPress posts, pages, and options as YAML files parsable by Jekyll
+ *
+ * @package    JekyllExporter
+ * @author     Ben Balter <ben.balter@github.com>
+ * @copyright  2013-2016 Ben Balter
+ * @license    GPLv3
+ * @link       https://github.com/benbalter/wordpress-to-jekyll-exporter/
+ */
+
+/**
+ * Usage:
+ *
+ *     $ php jekyll-export-cli.php > my-jekyll-files.zip
+ *
+ * Must be run in the wordpress-to-jekyll-exporter/ directory.
+ */
+include '../../../wp-load.php';
+include '../../../wp-admin/includes/file.php';
+require_once 'jekyll-exporter.php'; // Ensure plugin is "activated".
+
+if ( php_sapi_name() != 'cli' ) {
+	wp_die( 'Jekyll export must be run via the command line or administrative dashboard.' );
+}
+
+$jekyll_export = new Jekyll_Export();
+$jekyll_export->export();
